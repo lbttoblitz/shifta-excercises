fccaca8855a8af6c3898f2faea35c18bf31fbf3f
matiaso <matiaso@shifta.la>
2025-06-29 21:52:30 -0300

    se sube ejercicio consolidacion con poc de accesibilidad herramienta axe

---
+++ Exercises/Accesibility/Axe-accesibility-WebExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Axe_accesibility_WebExample.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
